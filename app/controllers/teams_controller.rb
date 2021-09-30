60d86d460ad2e99e54f9977b1ba089dc3f0728b3
EleonorSemevo <eleonorsemevo@gmail.com>
2021-09-30 10:00:14 +0100

    Les 3 problèmes résolus

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,65 @@
+class TeamsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_team, only: %i[show edit update destroy]
+
+  def index
+    @teams = Team.where('owner_id = ?',current_user.id)
+  end
+
+  def show
+    @working_team = @team
+    change_keep_team(current_user, @team)
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def edit; end
+
+  def create
+    @team = Team.new(team_params)
+    @team.owner = current_user
+    if @team.save
+      @team.invite_member(@team.owner)
+      redirect_to @team, notice: I18n.t('views.messages.create_team')
+    else
+      flash.now[:error] = I18n.t('views.messages.failed_to_save_team')
+      render :new
+    end
+  end
+
+  def update
+    if @team.update(team_params)
+      redirect_to @team, notice: I18n.t('views.messages.update_team')
+    else
+      flash.now[:error] = I18n.t('views.messages.failed_to_save_team')
+      render :edit
+    end
+  end
+
+  def destroy
+    @team.destroy
+    redirect_to teams_url, notice: I18n.t('views.messages.delete_team')
+  end
+
+  def dashboard
+    @team = current_user.keep_team_id ? Team.find(current_user.keep_team_id) : current_user.teams.first
+  end
+
+  def modify_leader
+    @working_team.owner_id = params[:new_leader]
+    @working_team.save
+    redirect_to team_path(@working_team)
+  end
+
+  private
+
+  def set_team
+    @team = Team.friendly.find(params[:id])
+  end
+
+  def team_params
+    params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id ]
+  end
+end
